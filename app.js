0f588a7b056c59bd109919cc076d35b38706a12e
lalomx <lalo0892@gmail.com>
2022-03-02 08:15:45 -0600

    clase en vivo

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // IIFE
 (async () => {
+  const Movie = require("./models/movie")
   const express = require("express")
   const app = express()
-
+  const movieRouter = require("./routes/movies")
   try {
-    app.use(express.json())
+    // await Movie.loadData()
+    console.log("se creo la tabla")
+    app.use(express.json()) // JSON parse
 
-    app.get("/", (rq, rs) => rs.send("Hola"))
+    app.use("/api/movies", movieRouter)
 
     app.listen(
       8080,
--- routes/movies.js
+++ routes/movies.js
@@ -1,23 +1,38 @@
 const { Router } = require("express")
+const Movie = require("../models/movie")
 
 const router = Router()
 
+// {
+//  get: (req, res) => {}
+// }
 router.get("/", async (req, res) => {
-
-  res.send("OK")
+  const movies = await Movie.getAll()
+  res.send(movies)
 })
 
 router.get("/:id", async (req, res) => {
-  res.send("OK")
+  const { id } = req.params
+  console.log("ID: ", id)
+  const movie = await Movie.getById(id)
+  res.send(movie)
 })
 
 router.put("/:id", async (req, res) => {
+  const { id } = req.params
+  const { body } = req
+
+  await Movie.update(id, body)
+
   res.sendStatus(200)
 })
 
 router.post("/", async (req, res) => {
-  res.status(201).send("OK")
+  const { body } = req
 
+  const id = await Movie.create(body)
+
+  res.status(201).send({ id })
 })
 router.delete("/:id", async (req, res) => {
 
